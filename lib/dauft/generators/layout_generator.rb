ba58e21e6878ce8048fafa47823c7330196ec2ce
michaelnera <mnera@clinic-it.com>
2013-09-05 16:17:21 +0800

    fix layout generator

--- lib/dauft/generators/layout_generator.rb
+++ lib/dauft/generators/layout_generator.rb
@@ -4,26 +4,18 @@ module Dauft
   module Generators
     class LayoutGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-      argument :actions, :type => :array, :default => ['application']
+      argument :layout_name, :type => :array, :default => 'application'
 
 
       def generate_layout
-        initialize_arguments
-
         template "layouts/#{file_name}.html.erb", "app/views/layouts/#{file_name}.html.erb"
       end
 
 
       private
       
-      def initialize_arguments
-        @layout_name = actions[0] if actions[0]
-        @controller_name = actions[1] if actions[1]
-      end
-
-
       def file_name
-        @layout_name.underscore.downcase
+        layout_name.underscore.downcase
       end
 
     end
