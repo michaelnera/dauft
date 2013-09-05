c8cf1c65e8a6aec7dcd6850a0eb6447e7b973f51
michaelnera <mnera@clinic-it.com>
2013-09-05 16:05:22 +0800

    remove sprockets; fix view generator

--- lib/dauft/engine.rb
+++ lib/dauft/engine.rb
@@ -1,7 +1,4 @@
 module Dauft
   class Engine < Rails::Engine
-    add_paths_block = lambda { |app|
-      app.config.assets.paths << File.expand_path("../../../scss", __FILE__)
-    }
   end
 end
--- lib/dauft/generators/view_generator.rb
+++ lib/dauft/generators/view_generator.rb
@@ -10,45 +10,18 @@ module Dauft
 
 
       def generate_views
-        initialize_arguments
-        generate_action_files
+        full_directory_path = File.join(Dauft.destination, '/', controller_name)
+
+        actions.each do |action|
+          copy_file "contents/#{template_name}.html.erb", "#{full_directory_path}/#{action}.html.erb"
+        end
       end
 
 
       private
       
-      def initialize_arguments
-        @full_directory_path = File.join(Dauft.destination, controller_name)
-        directory_path = controller_name.split('/')
-#        @controller = directory_path.pop
-#        @namespace = directory_path[0] if directory_path[0]
-        @namespace_folder = "#{Dauft.destination}/#{@namespace}"
-        @controller_folder = "#{Dauft.destination}/#{controller_name}"
-      end
-
-
       def template_name
         layout_name.underscore.downcase
-      end
-
-     
-      def generate_directory
-        return false unless File.directory?(@full_directory_path)
-
-        unless File.directory? @namespace_folder
-          Dir.mkdir @namespace_folder
-        end
-
-        unless File.directory? @controller_folder
-          Dir.mkdir @controller_folder
-        end
-      end
-
-
-      def generate_action_files
-        actions.each do |action|
-          copy_file "contents/#{template_name}.html.erb", "#{@full_directory_path}/#{action}.html.erb"
-        end
       end
 
     end
